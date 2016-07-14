c2a7fe8744636d45f9e06b6d84dc9aa7e1932f93
Nina Chaubal <nina@cupcakesystems.com>
2016-07-13 18:09:33 -0600

    Move styles to new dir. Add livereload

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var livereload = require('express-livereload')
+
 var app = express();
+livereload(app, config={});
+
 app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('index', {});
