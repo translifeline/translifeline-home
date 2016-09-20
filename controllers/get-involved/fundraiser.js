3c4ad6175ab14733fbde2982af69412ea0c844bf
Nina Chaubal <nina@cupcakesystems.com>
2016-09-20 00:09:54 -0500

    host a fundraiser

--- controllers/get-involved/fundraiser.js
+++ controllers/get-involved/fundraiser.js
@@ -5,12 +5,13 @@ const banner = require('../common/banner')
 
 /* A controller for the home page. */
 function controller(app) {
-  app.get('/fundraiser', function (req, res) {
+  app.get('/hostafundraiser', function (req, res) {
     banner(app).then(function(banner) {
       let data = {
         menu: menu(),
         showBanner: true,
-        banner: banner
+        banner: banner,
+        showSidebar: true
       };
       res.render('get-involved/fundraiser', data);
     });
