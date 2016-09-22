6b6f58009f8a73db44cdde161727ac49ea276e63
Nina Chaubal <nina@cupcakesystems.com>
2016-09-22 02:03:29 -0500

    Banner transition

--- client/banner.js
+++ client/banner.js
@@ -6,16 +6,18 @@ JS needed by banner features.
 function banner($) {
   let numImages = $('.banner ul').attr('data-num');
   $('#banner0').addClass('active');
-  let index = 0;
-  window.setInterval(
-    function() {
-      let nextIndex = (index + 1) % numImages;
-      $('#banner'  + index).removeClass('active');
-      $('#banner' + nextIndex).addClass('active');
-      index = nextIndex;
-    },
-    5000
-  );
+  if (numImages > 1) {
+    let index = 0;
+    window.setInterval(
+      function() {
+        let nextIndex = (index + 1) % numImages;
+        $('#banner'  + index).removeClass('active');
+        $('#banner' + nextIndex).addClass('active');
+        index = nextIndex;
+      },
+      5000
+    );
+  }
 
   $('.banner').height($('.banner img').height());
 }
