dea67d477b9a92de468a24031325e1e8e3288ece
Nina Chaubal <nina@cupcakesystems.com>
2017-02-18 14:46:06 -0800

    Change volunteer link

--- controllers/common/menu.js
+++ controllers/common/menu.js
@@ -12,7 +12,7 @@ function getMenuData() {
     new MenuItem('Survey', 'http://www.transmentalhealthsurvey.org'),
     new MenuItem('Get Involved', null, [
       new MenuItem('Ways to Help', '/help'),
-      new MenuItem('Volunteer', 'http://apply.translifeline.org'),
+      new MenuItem('Volunteer', 'https://jobs.lever.co/translifeline'),
       new MenuItem('Host A Fundraiser', '/hostafundraiser'),
       new MenuItem('Host Resources', '/host-resources'),
     ]),
