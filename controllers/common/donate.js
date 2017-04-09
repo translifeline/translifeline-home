dbeb0d80cfb48110a4b9d9d6541179e56ce107b2
Nina Chaubal <nina@cupcakesystems.com>
2017-04-08 20:26:13 -0700

    Add campaign progress based on stripe data

--- controllers/common/donate.js
+++ controllers/common/donate.js
@@ -1,8 +1,40 @@
 'use strict';
 
-function getDonateData(app) {
-  let db = app.get('database');
-  return db.collection('donate').findOne({});
-}
+let donateData = null;
+let lastUpdated = null;
 
-module.exports = getDonateData;
+module.exports = function(app) {
+  return new Promise((resolve, reject) => {
+    if (donateData && lastUpdated &&
+        Math.floor((Date.now() - lastUpdated) / (60000)) < 5) {
+      // Data was updated within the last 5 mins.
+      resolve(donateData);
+    } else {
+      let db = app.get('database');
+      db.collection('donate').findOne({}).then((donate) => {
+        let start = Date.parse(donate.start) / 1000;
+        let end = Date.parse(donate.end) / 1000;
+        db.collection('charges').aggregate([
+          { $match: { created: {$gte: start, $lt: end} } },
+          { $group: { _id: null, amountRaised: {$sum: '$amount'} } }
+        ]).next((err, result) => {
+          if(err) {
+            console.log(err);
+            reject(err);
+          } else {
+            let amountRaised = result.amountRaised / 100; // Stripe returns amounts in cents.
+            donate['progress'] = {
+              amountRaised: amountRaised,
+              matchedAmount: Math.max(donate.matchMax, amountRaised * donate.matchMultiplier),
+              percent: (amountRaised * 100) / donate.goal,
+              daysLeft: Math.floor((end - (Date.now()/1000))/(60*60*24))
+            }
+            donateData = donate;
+            lastUpdated = Date.now();
+            resolve(donateData);
+          }
+        });
+      });
+    }
+  });
+};
