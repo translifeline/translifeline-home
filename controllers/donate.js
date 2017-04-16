abb266defb3456802e39b872a68975cbc29947ef
Nina Chaubal <nina@cupcakesystems.com>
2017-04-15 19:38:29 -0700

    Add support for fundraising event donate links

--- controllers/donate.js
+++ controllers/donate.js
@@ -6,13 +6,19 @@ const donate = require('./common/donate');
 /* A controller for the donate page. */
 function controller(app) {
   app.get('/donate', function (req, res) {
-    donate(app).then(function(donate) {
-      let data = {
-        menu: menu(),
-        donate: donate
-      };
-      res.render('donate', data);
-    });
+    if(process.env.DONATE_URI) {
+      // If a donate uri is provided, redirect to that instead of using our
+      // default donate page. This is useful during fundraising events.
+      res.redirect(process.env.DONATE_URI);
+    } else {
+      donate(app).then(function(donate) {
+        let data = {
+          menu: menu(),
+          donate: donate
+        };
+        res.render('donate', data);
+      });
+    }
   });
 }
 
