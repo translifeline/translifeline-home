660b5572b9ec0c8bc7ed23236231933eda04db53
Nina Chaubal <ninachaubal@users.noreply.github.com>
2017-12-22 17:19:23 -0800

    Change default values in microgrants admin

--- controllers/admin/admin.js
+++ controllers/admin/admin.js
@@ -3,6 +3,7 @@
 const menu = require('../common/menu');
 const banner = require('../common/banner');
 const donate = require('../common/donate');
+const microgrants = require('../common/microgrants');
 const passport = require('passport');
 
 /* A controller for the admin page. */
@@ -10,13 +11,16 @@ function controller(app) {
   app.get('/admin', passport.authenticate('basic', { session: false }), function (req, res) {
     banner(app).then(function(banner) {
       donate(app).then(function(donate) {
-        let data = {
-          menu: menu(),
-          showBanner: false,
-          banner: banner,
-          donate: donate
-        };
-        res.render('admin/admin', data);
+        microgrants(app).then(function(donate) {
+          let data = {
+            menu: menu(),
+            showBanner: false,
+            banner: banner,
+            donate: donate,
+            microgrants: microgrants
+          };
+          res.render('admin/admin', data);
+        });
       });
     });
   });
